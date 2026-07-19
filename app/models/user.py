2fbd18e40908ab857b97512fae4102d994bd5538
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2026-07-19 19:09:26 +0530

    Created user model'

---
+++ app/models/user.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from werkzeug.security import generate_password_hash,check_password_hash
+from app.extensions import db
+
+class User:
+    __tablename__="users"
+
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(80),nullable=False)
+    email=db.Column(db.String(120),unique=True,nullable=False)
+    password=db.Column(db.String(255),nullable=False)
+    created_at=db.Column(db.Datetime,default=datetime.utcnow)
+
+    tasks=db.relationship(
+        "Task",
+        back_populates='user',
+        cascade="all, delete-orphan"
+    )
+
+    def set_password(self,password):
+        self.password=generate_password_hash(password)
+
+    def check_password(self,password):
+        return  check_password_hash(self.password,password)
+
