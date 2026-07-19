26cdd4bc40b825f117ed7260681d6910a1372eb0
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2026-07-19 19:23:55 +0530

    DB initialization completed

--- app/models/user.py
+++ app/models/user.py
@@ -9,7 +9,7 @@ class User(db.Model):
     username=db.Column(db.String(80),nullable=False)
     email=db.Column(db.String(120),unique=True,nullable=False)
     password=db.Column(db.String(255),nullable=False)
-    created_at=db.Column(db.Datetime,default=datetime.utcnow)
+    created_at=db.Column(db.DateTime,default=datetime.utcnow)
 
     tasks=db.relationship(
         "Task",
