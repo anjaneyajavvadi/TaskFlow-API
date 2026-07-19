95a22133f115da5b06b4567e9f4a7c329c95aebf
anjaneyajavvadi <anjaneyajavvadi@gmail.com>
2026-07-19 20:05:23 +0530

    implmented authentication routes

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -1,3 +1,52 @@
-from flask import Blueprint
+from flask import Blueprint,request,jsonify
+from flask_jwt_extended import create_access_token
+
+from app.extensions import db
+from app.models.user import User
+from app.schemas import RegisterSchema,LoginSchema,UserSchema
 
 auth_bp=Blueprint("auth",__name__)
+
+register_schema=RegisterSchema()
+login_schema=LoginSchema()
+user_schema=UserSchema()
+
+@auth_bp.post("/register")
+def register():
+    data=register_schema.load(request.json)
+
+    if(User.query.filter_by(email=data['email']).first()):
+        return jsonify({"message":"Email already Exists"}),400
+    
+    user=User(
+        username=data['username'],
+        email=data['email']
+    )
+
+    user.set_password(data['password'])
+
+    db.session.add(user)
+    db.session.commit()
+
+    return jsonify({
+        "message":"User registered successfully",
+        "user":user_schema.dump(user)
+    }),201
+
+@auth_bp.post("/login")
+def login():
+    data = login_schema.load(request.json)
+
+    user = User.query.filter_by(email=data["email"]).first()
+
+    if not user or not user.check_password(data["password"]):
+        return jsonify({"message": "Invalid email or password"}), 401
+
+    token = create_access_token(identity=str(user.id))
+
+    return jsonify({
+        "access_token": token,
+        "user": user_schema.dump(user)
+    }), 200
+
+    
